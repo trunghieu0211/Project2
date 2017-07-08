31210d0399877bad5dd3f302b5b9218f2bd40f39
TrungHieu <harrypotter12g@gmail.com>
2017-07-08 15:33:03 +0700

    create tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,14 @@ class Post < ApplicationRecord
   validates :user, presence: true
   validates :title, presence: true, length: {maximum: Settings.post.title_max_size}
   validates :content, presence: true
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+        Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
 end
